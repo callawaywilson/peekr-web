6960cfe6da311e2302300b73c6f7b958ebcf3913
Adam Wilson <adam@callawaywilson.com>
2013-02-04 19:03:15 -0500

    Added preview tester

---
+++ js/peekr_home.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $(".peekr").attachPeekr();
+  $("#peekr-preview").bind('submit', previewPeek);
+});
+
+function previewPeek(e) {
+  e.preventDefault();
+  var val = $("#peekUrl").val();
+  if (!val || /^\s*$/.test(val)) return false;
+  startPeek();
+  Peekr.data(val, function(data) {
+    endPeek();
+    renderPeekTooltip(data);
+    renderPeekData(data);
+  });
+  return false;
+}
+
+function startPeek() {
+  $(".btn-peek").addClass("disabled");
+  $(".btn-peek").text("Peeking...");
+}
+
+function endPeek() {
+  $(".btn-peek").removeClass("disabled");
+  $(".btn-peek").text("Peek!");
+}
+
+function renderPeekTooltip(data) {
+  $(".peekr-preview-tooltip").html("<div class='_peekr' style='position:relative'>"+Peekr.dataHtml(data)+"</div>");
+}
+
+function renderPeekData(data) {
+  $(".peekr-preview-data").html("<pre>"+JSON.stringify(data, null, '\t')+"</pre>");
+}
